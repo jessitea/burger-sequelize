f25fe12d1331e1748b457a69036de8bc27dd8ac3
jessitea <JessiPro@Jessicas-MacBook-Pro.local>
2017-07-06 15:13:56 -0400

    pre-sequelized files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured)VALUES ("Chicken Cheddar Burger", false);
+INSERT INTO burgers (burger_name, devoured)VALUES ("Big Mac", false);
+INSERT INTO burgers (burger_name, devoured)VALUES ("White Castle Slider", false);
